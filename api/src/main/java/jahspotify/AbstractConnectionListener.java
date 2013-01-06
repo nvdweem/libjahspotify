85f82a8243fbd24467294b40b0803922db3bf87d
Niels van de Weem <nielsgithub@vdweem.com>
2013-01-06 20:52:05 +0100

    AbstractConnectionListener also implements playlistsLoaded

--- api/src/main/java/jahspotify/AbstractConnectionListener.java
+++ api/src/main/java/jahspotify/AbstractConnectionListener.java
@@ -6,11 +6,12 @@ package jahspotify;
  */
 public abstract class AbstractConnectionListener implements ConnectionListener {
 
-	@Override public void initialized(boolean initialized) {}
+	@Override public void initialized(final boolean initialized) {}
 	@Override public void connected() {}
 	@Override public void disconnected() {}
-	@Override public void loggedIn(boolean success) {}
+	@Override public void loggedIn(final boolean success) {}
+	@Override public void playlistsLoaded(final boolean contents) {}
 	@Override public void loggedOut() {}
-	@Override public void blobUpdated(String blob) {}
+	@Override public void blobUpdated(final String blob) {}
 
 }
